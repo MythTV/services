b5e7092fab5647290d3913c14cbd14b6d903d172
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-04-12 00:11:01 +0100

    Basic search by callsign

--- app/controllers/channel_icon_controller.rb
+++ app/controllers/channel_icon_controller.rb
@@ -28,9 +28,31 @@ class ChannelIconController < ApplicationController
   end
 
   def find_missing
+    # param csv= which has the following data escaped
+    # - ChannelId,
+    # - Name, Xmltvid, Callsign, TransportId, AtscMajorChan, AtscMinorChan,
+    # - NetworkId, ServiceId
   end
 
   def search
+    # param s= with a callsign. eg.
+    # - s=BBC+One+HD
+    # - s=BBC%20One%20HD
+    # param csv= which has the following data escaped
+    # - Name, Xmltvid, Callsign, TransportId, AtscMajorChan, AtscMinorChan,
+    # - NetworkId, ServiceId
+    if params[:s]
+      ################################
+      # NOTE: This in theory should lookup the callsign table, but the existing
+      # code does not do this, so initially implement a compatible lookup
+      # - Find the callsigns
+      #callsigns = ChannelIcon::Callsign.where("callsign LIKE ?", "%#{params[:s]}%")
+      # - Find the icons from the callsigns
+      #@icons = ChannelIcon::Icon.find_by_icon_id(callsigns)
+      ################################
+      # TODO: Multi pass searching
+      @icons = ChannelIcon::Icon.where("enabled = 1 AND name LIKE ?", "%#{params[:s]}%")
+    end
   end
 
   def master_iconmap
