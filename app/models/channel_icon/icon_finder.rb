29e6f23b3161248eb566f0280b0351e34f5662a0
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-04-20 16:08:58 +0100

    Convert xmltvid to lowercase

--- app/models/channel_icon/icon_finder.rb
+++ app/models/channel_icon/icon_finder.rb
@@ -15,7 +15,7 @@ class ChannelIcon::IconFinder
   def find_by_xmltvid(q)
     iconQuery = ChannelIcon::IconRecord.new
     iconQuery.query = 'xmltvid'
-    xmltvid = ChannelIcon::Xmltvid.find_by_xmltvid(q)
+    xmltvid = ChannelIcon::Xmltvid.find_by_xmltvid(q.downcase)
     if !xmltvid.nil?
       @icon = iconQuery.find_by_icon_id(xmltvid.icon_id)
     else
