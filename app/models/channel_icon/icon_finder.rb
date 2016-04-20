6205569390cefe7fa5dff8ed96b8015bf57dd8c7
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-04-20 09:34:38 +0100

    Ensure query method is set for failing queries

--- app/models/channel_icon/icon_finder.rb
+++ app/models/channel_icon/icon_finder.rb
@@ -2,24 +2,24 @@
 # via the various different methods.
 class ChannelIcon::IconFinder
   def find_by_callsign(q)
+    iconQuery = ChannelIcon::IconRecord.new
+    iconQuery.query = 'callsign'
     callsign = ChannelIcon::Callsign.find_by_callsign(q.downcase)
     if !callsign.nil?
-      iconQuery = ChannelIcon::IconRecord.new
-      iconQuery.query = 'callsign'
       @icon = iconQuery.find_by_icon_id(callsign.icon_id)
     else
-      @icon = ChannelIcon::IconRecord.new.error_callsign
+      @icon = iconQuery.error_callsign
     end
   end
 
   def find_by_xmltvid(q)
+    iconQuery = ChannelIcon::IconRecord.new
+    iconQuery.query = 'xmltvid'
     xmltvid = ChannelIcon::Xmltvid.find_by_xmltvid(q)
     if !xmltvid.nil?
-      iconQuery = ChannelIcon::IconRecord.new
-      iconQuery.query = 'xmltvid'
       @icon = iconQuery.find_by_icon_id(xmltvid.icon_id)
     else
-      @icon = ChannelIcon::IconRecord.new.error_xmltvid
+      @icon = iconQuery.error_xmltvid
     end
   end
 end
