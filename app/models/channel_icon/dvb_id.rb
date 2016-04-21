beda5f0815434379f84f9c67ca617a8647e08aa2
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-04-21 12:01:01 +0100

    Add model for dvb_ids table

---
+++ app/models/channel_icon/dvb_id.rb
@@ -0,0 +1,9 @@
+class ChannelIcon::DvbId < ActiveRecord::Base
+  self.primary_key = 'networkid'
+  belongs_to  :icon
+  
+  scope :find_by_dvb_tuple, ->(netid,tsid,serviceid) {
+      where("networkid = ? and transportid = ? and serviceid = ?",
+        "#{netid}", "#{tsid}", "#{serviceid}")
+    }
+end
