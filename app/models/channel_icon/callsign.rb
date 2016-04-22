1c300e98ac2ddfd92efa1e702e561046a5cda1e3
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-04-22 09:57:20 +0100

    Remove cruft from Callsign model

--- app/models/channel_icon/callsign.rb
+++ app/models/channel_icon/callsign.rb
@@ -1,16 +1,3 @@
 class ChannelIcon::Callsign < ActiveRecord::Base
   belongs_to  :icon
-  def fullUrl
-    icon = ChannelIcon::Icon.find_by_icon_id(self.icon_id)
-    source = ChannelIcon::Source.find_by_source_id(icon.source_id)
-    @fullUrl = source.url + '/' + icon.icon
-  end
-  def iconID
-    icon = ChannelIcon::Icon.find_by_icon_id(self.icon_id)
-    @iconID = icon.icon_id.to_s
-  end
-  def iconName
-    icon = ChannelIcon::Icon.find_by_icon_id(self.icon_id)
-    @iconName = icon.name
-  end
 end
