2a40701354c78c4887a55123dbbe1ba0f9e7325b
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-05-24 17:02:12 +0100

    Basic tests for sources model

--- app/models/channel_icon/source.rb
+++ app/models/channel_icon/source.rb
@@ -1,3 +1,5 @@
 class ChannelIcon::Source < ActiveRecord::Base
   has_many :icons
+  validates :name, presence: true
+  validates :url, presence: true
 end
--- test/models/source_test.rb
+++ test/models/source_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class SourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "source missing name and url" do
+    source = ChannelIcon::Source.new
+    assert_not source.save
+  end
+  test "source missing url" do
+    source = ChannelIcon::Source.new(name: "bob")
+    assert_not source.save
+  end
+  test "source missing name" do
+    source = ChannelIcon::Source.new(url: "http://example.com")
+    assert_not source.save
+  end
+  test "valid source should save" do
+    source = ChannelIcon::Source.new(name: "bob", url: "http://example.com")
+    assert source.save
+  end
 end
