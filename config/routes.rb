b0d6d0b124756e6ea4b86ec502b994fd79de1f67
Stuart Auchterlonie <stuarta@mythtv.org>
2022-04-19 23:06:19 +0100

    Allow options requests on certain routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  match 'channel-icon/checkblock', to: 'channel_icon#check_block', via: [:get, :post ]
-  match 'channel-icon/findmissing', to: 'channel_icon#find_missing', via: [:get, :post ]
-  match 'channel-icon/lookup', to: 'channel_icon#lookup', via: [:get, :post ]
+  match 'channel-icon/checkblock', to: 'channel_icon#check_block', via: [:get, :post, :options ]
+  match 'channel-icon/findmissing', to: 'channel_icon#find_missing', via: [:get, :post, :options ]
+  match 'channel-icon/lookup', to: 'channel_icon#lookup', via: [:get, :post, :options ]
   match 'channel-icon/master-iconmap', to: 'channel_icon#master_iconmap', via: [:get, :head ]
   match 'channel-icon/ping', to: 'channel_icon#ping', via: [:get ]
-  match 'channel-icon/search', to: 'channel_icon#search', via: [:get, :post ]
+  match 'channel-icon/search', to: 'channel_icon#search', via: [:get, :post, :options ]
   match 'channel-icon/sources', to: 'channel_icon/sources#index', via: [:get, :head ]
-  match 'channel-icon/submit', to: 'channel_icon#submit', via: [:post ]
+  match 'channel-icon/submit', to: 'channel_icon#submit', via: [:post, :options ]
   match 'channel-icon/edit', to: 'channel_icon/editor#index', via: [:get]
 
   namespace :music do
