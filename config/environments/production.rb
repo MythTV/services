81fb194691fe95d436a9b61de39b2cc394ae35ec
David Hampton <mythtv@love2code.net>
2025-09-28 20:27:37 -0400

    Log to file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,9 +33,10 @@ Rails.application.configure do
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
-  # Log to STDOUT with the current request id as a default log tag.
+  # Log to file with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger   = ActiveSupport::TaggedLogging.logger("log/#{Rails.env}.log")
+  #config.logger  = ActiveSupport::TaggedLogging.logger(STDOUT)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
